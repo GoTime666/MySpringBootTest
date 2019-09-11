5f32b0a9148fc29e9f6842286a226617eced2d49
GoTime666 <GoTime@DESKTOP-8DCD47Q>
2019-09-11 14:36:35 +0800

    增加2个线程

--- ThreadTest/src/thread/ThreadDemo.java
+++ ThreadTest/src/thread/ThreadDemo.java
@@ -1,5 +1,10 @@
 package thread;
 
 public class ThreadDemo {
-	
+	public static void main(String[] args) {
+		MyThread1 t1=new MyThread1("我的线程1");
+		t1.start();
+		for(int i=1;i<100;i++)
+			System.out.println("主线程:   "+i);
+	}
 }
