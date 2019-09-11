7221632941d558704b8dd743a162da1dda2efa59
GoTime666 <GoTime@DESKTOP-8DCD47Q>
2019-09-11 14:45:00 +0800

    更新runnable

---
+++ ThreadTest/src/runnable/MyRunnable.java
@@ -0,0 +1,12 @@
+package runnable;
+
+public class MyRunnable implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		for(int i=1;i<100;i++)
+			System.out.println(i);
+	}
+
+}
